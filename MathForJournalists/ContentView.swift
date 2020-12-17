cb1421eb2e2b6d73bd5f3da0134c0e6600321f7e
Girish Gupta <girish@girishgupta.com>
2020-12-17 09:18:45 +0100

    Initial Commit

---
+++ MathForJournalists/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MathForJournalists
+//
+//  Created by Girish Gupta on 17/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MathForJournalists/MathForJournalistsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathForJournalistsApp.swift
+//  MathForJournalists
+//
+//  Created by Girish Gupta on 17/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct MathForJournalistsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
