9c1318a143ff3f19969c5b66f8009c2783746b9b
Girish Gupta <girish@girishgupta.com>
2020-12-30 09:39:37 +0100

    Keyboard pretty much sorted

--- MathForJournalists/ContentView.swift
+++ MathForJournalists/ContentView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct ContentView: View
 {
     @State private var SelectedTab = 0
-    @State private var Title = "Percentages"
     
     var body: some View
     {
@@ -26,12 +25,31 @@ struct ContentView: View
                 Text("Tab Content 4").tabItem {
                     Image(systemName: "square.and.pencil");
                     Text("Surveys/Polls").tag(3) }
-                Text("About").tabItem {
+                About().tabItem {
                     Image(systemName: "info");
                     Text("About").tag(4) }
             }
 //            .foregroundColor(.black)
             .accentColor(.orange)
+        }
+    }
+}
+
+struct About: View
+{
+    var body: some View
+    {
+        NavigationView
+        {
+            VStack
+            {
+                Text("Math for Journalists was created by Girish Gupta, a former investigative and multimedia journalist working in Venezuela, the Americas and Middle East with everyone from Reuters to the New Yorker.\n\nGirish has a Master's in physics and two decades of full-stack programming experience.\n\nThis iOS app was created as a quick final project for Harvard's excellent CS50 computer science course.").padding()
+                Link(destination: URL(string: "https://www.girishgupta.com/")!) {
+                    Image(systemName: "link")
+    //                    .font(.largeTitle)
+                    Text("girishgupta.com")
+                }.padding() // https://www.hackingwithswift.com/quick-start/swiftui/how-to-open-web-links-in-safari
+            }.navigationBarTitle(Text("About"))
         }
     }
 }
@@ -43,7 +61,6 @@ struct Percentages: View
     @State private var old: Double = 5
     @State private var new: Double = 8
     
-    
     var calc: Double { 100 * (new - old) / old } // https://stackoverflow.com/a/58140902
     
     var body: some View
@@ -54,39 +71,49 @@ struct Percentages: View
             {
                 VStack
                 {
-                    Text("Calculating percentage differences or changes between two values is a fundamental means of explaining complex figures.").padding()
+                    Text("Calculating percentage changes between two values is a fundamental means of explaining changes in time.\n\nThis could apply to anything from budgets to prices.\n\nDon't forget to check the sign!").padding()
                     
-                    Toggle(isOn: $time)
-                    {
-                        Text("Firstly, is there a time-element here?")
-                    }.padding() // https://www.hackingwithswift.com/quick-start/swiftui/how-to-create-a-toggle-switch
+//                    Toggle(isOn: $time)
+//                    {
+//                        Text("Firstly, is there a time-element here?")
+//                    }.padding() // https://www.hackingwithswift.com/quick-start/swiftui/how-to-create-a-toggle-switch
+//
+//                    if (time)
+//                    {
+//                        Text("There's a time element so you're going to show that the old value has risen/fallen X%. ").padding()
+//                    }
+//                    else
+//                    {
+//                        Text("There's no time element so we're showing that one value is X% of another.").padding()
+//                    }
                     
-                    if (time)
-                    {
-                        Text("There's a time element so you're going to show that the old value has risen/fallen X%. ").padding()
-                    }
-                    else
-                    {
-                        Text("There's no time element so we're showing that one value is X% of another.").padding()
-                    }
-                    
-                    HStack
+                    HStack(alignment: .center)
                     {
                         VStack
                         {
-                            Text(time ? "Old Value" : "First Value").padding()
-                            Text(time ? "New Value" : "Second Value").padding()
+                            Text("Old Value").padding()
+                            Text("New Value").padding()
                         }
                         
                         VStack
                         {
-                            
-                            TextField("", value: $old, formatter: NumberFormatter()) // https://www.simpleswiftguide.com/swiftui-textfield-complete-tutorial/ https://stackoverflow.com/a/59509242
+//                            TextField("", value: $old, formatter: NumberFormatter()) // https://www.simpleswiftguide.com/swiftui-textfield-complete-tutorial/ https://stackoverflow.com/a/59509242
+                            TextField("", text: Binding<String>(
+                                get: { String(format: "%.1f", self.old) },
+                                set: {
+                                    if let value = NumberFormatter().number(from: $0) {
+                                        self.old = value.doubleValue
+                                    }})) // https://stackoverflow.com/a/61270145
                                 .textFieldStyle(RoundedBorderTextFieldStyle())
                                 .keyboardType(.decimalPad) // https://programmingwithswift.com/numbers-only-textfield-with-swiftui/
                                 .padding()
                             
-                            TextField("New Value", value: $new, formatter: NumberFormatter()) // https://www.simples)wiftguide.com/swiftui-textfield-complete-tutorial/
+                            TextField("", text: Binding<String>(
+                                get: { String(format: "%.1f", self.new) },
+                                set: {
+                                    if let value = NumberFormatter().number(from: $0) {
+                                        self.new = value.doubleValue
+                                    }})) // https://stackoverflow.com/a/61270145
                                 .textFieldStyle(RoundedBorderTextFieldStyle())
                                 .keyboardType(.decimalPad) // https://programmingwithswift.com/numbers-only-textfield-with-swiftui/
                                 .padding()
@@ -96,11 +123,24 @@ struct Percentages: View
                 
                 VStack
                 {
-                    Text("\(self.calc, specifier: "%.1f") %") //https://www.hackingwithswift.com/quick-start/swiftui/formatting-interpolated-strings-in-swiftui
-                        .font(.system(size: 60))
+                    HStack(alignment: .center)
+                    {
+                        Text("\(self.calc, specifier: "%.0f")") //https://www.hackingwithswift.com/quick-start/swiftui/formatting-interpolated-strings-in-swiftui
+                            .font(.system(size: 60))
+                        Text("%")
+                    }.padding()
                     
-                }.navigationBarTitle(Text("Percentages"))
-            }
+                    if (self.calc<0)
+                    {
+                        Text("[Subject] is down \(abs(self.calc), specifier: "%.0f") per cent in [time period].").padding()
+                    }
+                    else
+                    {
+                        Text("[Subject] is up \(abs(self.calc), specifier: "%.0f") per cent in [time period].").padding()
+                    }
+                }
+            }.navigationBarTitle(Text("Percentage Change"))
+            .gesture(TapGesture().onEnded{_ in UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)}) // https://stackoverflow.com/a/59704606 https://developer.apple.com/documentation/swiftui/tapgesture (https://www.hackingwithswift.com/quick-start/swiftui/how-to-dismiss-the-keyboard-for-a-textfield)
         }
     }
 }
